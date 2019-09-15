442d2f8c1c4f8721649c6daf98ecc8b99cc0e3e9
Jay Shollenberger <jshollen@jshollen.remote.fedora>
2019-09-15 10:25:42 -0600

    Initial Commit

---
+++ bin/setup_jenkins.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Setup Jenkins Project
+if [ "$#" -ne 3 ]; then
+    echo "Usage:"
+    echo "  $0 GUID REPO CLUSTER"
+    echo "  Example: $0 wkha https://github.com/redhat-gpte-devopsautomation/advdev_homework_template.git na311.openshift.opentlc.com"
+    exit 1
+fi
+
+GUID=$1
+REPO=$2
+CLUSTER=$3
+echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
+
+# Set up Jenkins with sufficient resources
+# TBD
+# Mine
+oc new-project ${GUID}-jenkins
+oc new-app ${GUID}-jenkins --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
+oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m
+
+# Create custom agent container image with skopeo
+# TBD
+# Mine
+oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
+      USER root\nRUN yum -y install skopeo && yum clean all\n
+      USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
+
+# Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
+# TBD
+
+# Make sure that Jenkins is fully up and running before proceeding!
+while : ; do
+  echo "Checking if Jenkins is Ready..."
+  AVAILABLE_REPLICAS=$(oc get dc jenkins -n ${GUID}-jenkins -o=jsonpath='{.status.availableReplicas}')
+  if [[ "$AVAILABLE_REPLICAS" == "1" ]]; then
+    echo "...Yes. Jenkins is ready."
+    break
+  fi
+  echo "...no. Sleeping 10 seconds."
+  sleep 10
+done
