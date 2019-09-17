db43cabdfd1437acdc329a71a19d3a2032729d88
Jay Shollenberger <jshollen@jshollen.remote.fedora>
2019-09-16 19:31:00 -0500

    starting JenkinsFile

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -17,7 +17,6 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # Mine
 oc new-project ${GUID}-jenkins
 oc new-app ${GUID}-jenkins --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
-
 oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=1
 
 # Create custom agent container image with skopeo
