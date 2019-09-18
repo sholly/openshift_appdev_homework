4e5cb07706ae079583510b3ac61ee98acdbfcbde
Jay Shollenberger <jshollen@jshollen.remote.fedora>
2019-09-17 22:13:42 -0500

    new-app jenkins-persistent duh

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -17,7 +17,7 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # TBD
 # Mine
 # oc new-project ${GUID}-jenkins
-oc new-app jshollen-jenkins --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n "${GUID}-jenkins"
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n "${GUID}-jenkins"
 # oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=1
 
 # Create custom agent container image with skopeo
