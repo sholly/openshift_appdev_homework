a515b619eab40eb7fdc5db12fd74cee15427c0d0
Jay Shollenberger <jshollen@jshollen.remote.fedora>
2019-09-17 21:31:50 -0500

    fixes

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -15,8 +15,8 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # Set up Jenkins with sufficient resources
 # TBD
 # Mine
-oc new-project ${GUID}-jenkins
-oc new-app ${GUID}-jenkins --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
+# oc new-project ${GUID}-jenkins
+oc new-app jshollen-jenkins --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n "${GUID}-jenkins"
 oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=1
 
 # Create custom agent container image with skopeo
@@ -24,7 +24,7 @@ oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=
 # Mine
 oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
       USER root\nRUN yum -y install skopeo && yum clean all\n
-      USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
+      USER 1001' --name=jenkins-agent-appdev -n "${GUID}-jenkins"
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
 # TBD
